b3643be1f6ce8f6a81fab84d220d03afef9da962
danmandel <danmandel@users.noreply.github.com>
2015-08-01 16:11:53 -0400

    Update TwitterFeed.py

--- TwitterFeed.py
+++ TwitterFeed.py
@@ -1,6 +1,7 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import time
 
 ckey = 'oIkifLfqNy9ubpr3gJPCpEMAM'
 csecret = 'IqAyLmnmw5TXsBtCBM1G8UTUsPsngqX8vJL8WmYhdyZ3VhnS8L'
@@ -10,8 +11,16 @@ asecret = 'bnDehlfEE1W3ZXhJb0wckakrSC6yYHYE67TW4sH7VbYSr'
 class listener(StreamListener):
 
     def on_data(self, data):
-        print (data)
-        return True
+        try:
+            print (data)
+            saveFile = open('twitdb.csv','a')
+            saveFile.write(data)
+            saveFile.write('\n')
+            saveFile.close()  
+            return True
+        except BaseException, e:
+            print 'failed ondata, ', str(e)
+            time.sleep(5)
     
     def on_error(self, status):
         print (status)
@@ -19,4 +28,4 @@ class listener(StreamListener):
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
+twitterStream.filter(track=["gilinsky"])
